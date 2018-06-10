203037c5a99baa157511e56ac7952873a9a0ca7f
Edgar Villegas Alvarado <edgarinvillegas@hotmail.com>
2018-06-10 18:59:35 -0400

    chore: Remove mockup files

--- mockup/MainMockupDonator.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from 'react';
-import { Badge, Button, Container, Form, FormGroup, Input, Label, ListGroup, ListGroupItem, Row, } from 'reactstrap';
-
-export default () => (
-  <Fragment>
-
-  </Fragment>
-);
-
